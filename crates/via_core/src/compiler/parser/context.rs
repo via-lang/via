9253fbf1ba09186a1b1b42d6487402cacc6731f9
XnLogicaL <acceltemen@gmail.com>
2026-01-12 12:08:10 +0300

    fix(parser): context module

---
+++ crates/via_core/src/compiler/parser/context.rs
@@ -0,0 +1,38 @@
+/* ================================================ **
+**           The via Programming Language           **
+** ------------------------------------------------ **
+**        Copyright (C) XnLogicaL 2024-2026         **
+**           Licensed under GNU GPL v3.0            **
+** ------------------------------------------------ **
+**         https://github.com/via-lang/via          **
+** ================================================ */
+
+#[derive(Debug, Clone, Copy)]
+pub enum Context {
+    ExprTuple,
+    ExprGroup,
+    ExprLambda,
+    ExprBody,
+    ExprPrimary,
+    ExprPostfix,
+    TypeMap,
+    TypeLambda,
+    TypeId,
+    ControlReturn,
+    ControlRaise,
+    ControlIf,
+    ControlElseIf,
+    ControlElse,
+    ControlWhile,
+    ControlFor,
+    DeclVariable,
+    DeclFunction,
+    DeclType,
+    DeclConst,
+    DeclStruct,
+    DeclImport,
+    Body,
+    ReturnType,
+    ParameterList,
+    ArgumentList,
+}
